ffa73ac17210b616648bc9c7f04abaee3a9d300a
Leonid Usatii <leonid.usatiy@gmail.com>
2023-07-15 14:44:24 +0200

    add chenges

---
+++ script.js
@@ -0,0 +1,8 @@
+const fruits = ['apple', 'orange', 'banana'];
+const reversedFruits = fruits.reverse();
+console.log(fruits);
+console.log(reversedFruits);
+reversedFruits[0] = 'boot';
+
+console.log(fruits);
+console.log(reversedFruits);
